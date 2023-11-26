71cd4a47908f9c9f1456172ce30ddc9cb746188b
vikashkumar <vikashpoddar437@gmail.com>
2023-11-26 22:44:53 +0530

    happy number

---
+++ Happy Number.cpp
@@ -0,0 +1,57 @@
+// /////////////////////////// Question////////////////////////////
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+ 
+
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+
+// Input: n = 2
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= n <= 231 - 1
+////////////////////////////solution///////////////////////////
+class Solution {
+public:
+    bool isHappy(int n) {
+  set<int> s;
+        int val;
+        int index;
+        while(1){
+            val=0;
+            while(n){
+                index=n%10;
+                val+=index*index;
+                n/=10;
+            }
+            if(val==1){
+                return true;
+            }
+            else if(s.find(val)!=s.end()){
+                return false;}
+                s.insert(val);
+                n=val;
+                
+            
+        }
+        return false;
+    }
+};
