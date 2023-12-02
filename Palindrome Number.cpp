afac1d03d870b0fc795ebbf9774a354da6caa7de
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2023-12-03 00:47:14 +0530

    Create  Palindrome Number.cpp

---
+++ Palindrome Number.cpp
@@ -0,0 +1,45 @@
+/////////////////////////////////////Question///////////////////////////////////////
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+////////////////////////////////////solution//////////////////////////////////////
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        unsigned int temp,reverse=0;;
+     
+        temp=x;
+        while(temp>0){
+            int remender=temp%10;
+            reverse=reverse*10+remender;
+            temp/=10;
+        }
+        
+        return reverse==x;
+       
+    }
+};
