311b6cc4e806e8f69bb19e5a227fcec92fde034b
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-03-02 21:39:47 +0530

    Create Maximum Product Subarray.cpp

---
+++ Maximum Product Subarray.cpp
@@ -0,0 +1,41 @@
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+// Example 2:
+
+// Input: nums = [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -10 <= nums[i] <= 10
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+////////////////////////////////////////////////////////////////////////////////////////////////solution//////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int prefix=1;
+        int suffix=1;
+        int ans=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+         if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            ans=max(ans,max(suffix,prefix));
+        }
+        return ans;
+    }
+};
