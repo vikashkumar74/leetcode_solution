5aea40ecd61e0e51e72f9a8fadd5cc594874b3ef
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-03-04 23:22:25 +0530

    Create Unique Number of Occurrence.cpp

---
+++ Unique Number of Occurrence.cpp
@@ -0,0 +1,54 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 1000
+// -1000 <= arr[i] <= 1000
+//////////////////////////////////////////////////////////////////////////////solution/////////////////////////////////////////////////////
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector<int>ans;
+        int size =arr.size();
+        int i=0;
+        sort(arr.begin(),arr.end());
+        while(i<size){
+            int count=1;
+            for(int j=i+1; j<size; j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+            ans.push_back(count);
+            i=i+count;
+        }
+        size=ans.size();
+        sort(ans.begin(),ans.end());
+        for(int i=0; i<size-1; i++){
+            if(ans[i]==ans[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
