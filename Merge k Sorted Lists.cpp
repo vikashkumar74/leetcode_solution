ac746bbd779027adbe6c573d970d2d4eeca97c16
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-03-29 19:22:08 +0530

    Create  Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,74 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+// Example 3:
+
+// Input: lists = [[]]
+// Output: []
+ 
+
+// Constraints:
+
+// k == lists.length
+// 0 <= k <= 104
+// 0 <= lists[i].length <= 500
+// -104 <= lists[i][j] <= 104
+// lists[i] is sorted in ascending order.
+// The sum of lists[i].length will not exceed 104.
+
+//////////////////////////////////////////////////////////////////////solution//////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>> pq;
+            for(int i=0;i<lists.size();i++){
+                if(lists[i]){
+                pq.push({lists[i]->val,lists[i]});
+                }
+            }
+            ListNode*dummyNode=new ListNode(-1);
+            ListNode*temp=dummyNode;
+            while(!pq.empty()){
+                auto it=pq.top();
+                pq.pop();
+                if(it.second->next)
+                pq.push({it.second->next->val,it.second->next});
+                temp->next=it.second;
+                temp=temp->next;
+
+                
+            }
+   
+     return dummyNode->next;
+    }
+};
