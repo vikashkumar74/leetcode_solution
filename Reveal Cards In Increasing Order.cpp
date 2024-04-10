f6893d2f46bd64909d90d7cde44450103248e5e0
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-04-10 23:37:07 +0530

    Create Reveal Cards In Increasing Order.cpp

---
+++ Reveal Cards In Increasing Order.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(), deck.end());
+        deque<int> result;
+        for (int i = deck.size() - 1; i >= 0; --i) {
+            if (!result.empty()) {
+                result.push_front(result.back());
+                result.pop_back();
+            }
+            result.push_front(deck[i]);
+        }
+        return vector<int>(result.begin(), result.end());
+    }
+};
