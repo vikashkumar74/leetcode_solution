687fde094b6172ff85328158d7f673f199b530d6
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-05-01 00:08:16 +0530

    Update Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -6,26 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-TreeNode*tree(TreeNode*preorder,startpre,endpre,TreeNode*inorder,startin,endin,unordered_map<int,int>mapp){
-    if(startpre>endpre||startin>endin)
-    return nullptr;
-   int root=preorder[startpre];
-    TreeNode*root=new TreeNode(root);
-    int findNode=mapp[root];
-    int numLeft=findNode-startin;
-    root->left=tree(preorder,startpre+1,startpre+numLeft,inorder,startin,findNode-1,mapp);
-  root->left=tree(preorder,startpre+numLeft+1,endpre,inorder,findNode+1,endin,mapp);
-}
+    TreeNode* tree(vector<int>& preorder, int startpre, int endpre,
+                   vector<int>& inorder, int startin, int endin,
+                   unordered_map<int,int>& mapp) {
+        if (startpre > endpre || startin > endin)
+            return nullptr;
+        int rootnum = preorder[startpre];
+        TreeNode* root = new TreeNode(rootnum);
+        int findNode = mapp[rootnum];
+        int numLeft = findNode - startin;
+        root->left = tree(preorder, startpre + 1, startpre + numLeft, inorder,
+                          startin, findNode - 1, mapp);
+        root->right = tree(preorder, startpre + numLeft + 1, endpre, inorder,
+                          findNode + 1, endin, mapp);
+        return root;
+    }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int,int>mapp;
-        for(int i=0;i<inorder.size();i++){
-            mapp[inorder[i]]=i;
+        unordered_map<int, int> mapp;
+        for (int i = 0; i < inorder.size(); i++) {
+            mapp[inorder[i]] = i;
         }
-     return tree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mapp);
+        return tree(preorder, 0, preorder.size() - 1, inorder, 0,
+                    inorder.size() - 1, mapp);
     }
 };
