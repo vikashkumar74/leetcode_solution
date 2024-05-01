6434777915bcd324cab9a596b5141337b5af4d5d
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-05-01 23:15:17 +0530

    Create Reverse Prefix of Word.cpp

---
+++ Reverse Prefix of Word.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+      int index=word.find(ch);
+      reverse(word.begin(),word.begin()+index+1);
+      return word;  
+    }
+};
