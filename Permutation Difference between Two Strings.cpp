e93a1bb7b332dac69d52777f8eee4694be866b5f
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-05-12 22:21:42 +0530

    Create Permutation Difference between Two Strings.cpp

---
+++ Permutation Difference between Two Strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        int sum=0;
+        for(int i=0;i<s.size();i++){
+            for(int j=0;j<t.size();j++){
+                if(s[i]==t[j]){
+                    sum+=abs(i-j);
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
+};
