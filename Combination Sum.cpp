d1243293d1dcfd908b91ab02bf614b059508d3fe
Vikash kumar <126346486+vikashkumar74@users.noreply.github.com>
2024-05-30 23:59:53 +0530

    Create Combination Sum.cpp

---
+++ Combination Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  void backtrack(vector<int>& candidates, int target, vector<vector<int>>& results, vector<int>& combination, int start) {
+        if (target == 0) {
+
+            results.push_back(combination);
+            return;
+        }
+        
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > target) continue; 
+            combination.push_back(candidates[i]);
+
+            backtrack(candidates, target - candidates[i], results, combination, i);
+            combination.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>result;
+        vector<int> combination;
+        backtrack(candidates, target, result, combination, 0);
+        return result;
+
+        
+    }
+};
